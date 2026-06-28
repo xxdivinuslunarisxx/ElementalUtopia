1b9b4c617c142d839047d9386644f3da861f9c24
Your Name <you@example.com>
2026-06-28 21:09:29 +0100

    initial working version

---
+++ Models/Experiment.cs
@@ -0,0 +1,11 @@
+namespace ElementalUtopia.Models
+{
+    public class Experiment
+    {
+        public string Id { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Meta { get; set; } = "";
+        public string Summary { get; set; } = "";
+        public string Tag { get; set; } = "";
+    }
+}
