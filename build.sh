74e877a497c7a6134a2ab2dd772ed4776e5bcef5
Your Name <you@example.com>
2026-06-29 13:50:56 +0100

    x

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 
 ./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
 
-./dotnet/dotnet publish -c Release -o publish/wwwroot
+mkdir -p out
+
+./dotnet/dotnet publish -c Release -o out
