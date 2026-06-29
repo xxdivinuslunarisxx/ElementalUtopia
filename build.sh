99e6058f8f0b719039f9ec942a80823602301e44
Your Name <you@example.com>
2026-06-29 13:55:54 +0100

    fix cloudflare publish output directory

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-set -e
-
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 
 ./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
 
-mkdir -p out
-
-./dotnet/dotnet publish -c Release -o out
+./dotnet/dotnet publish -c Release -o wwwroot
