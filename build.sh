d5c36c31938e7af34ee1aee17001175788e48db9
Your Name <you@example.com>
2026-06-30 10:48:37 +0100

    Fix Cloudflare Pages Blazor build script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
 
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 
 ./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
 
-./dotnet/dotnet publish -c Release -o wwwroot
+./dotnet/dotnet publish -c Release -o output
